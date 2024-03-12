edd4f305cfb35e2804a99053c41d06a6b706fbbd
AshmiyaV <ashmiyavs6767@gmail.com>
2024-03-11 23:26:15 -0400

    MIView 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import HomeLoginPage from './pages/HomeLoginPage.jsx'
 import CandidateInfo from './pages/CandidateInfo.jsx';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import VapeView from './pages/VapeView.jsx';
+import DrugsView from './pages/DrugsView.jsx';
+import MIView1 from './pages/MIView1.jsx';
+import MIView2 from './pages/MIView2.jsx';
 
 function App() {
   return (
@@ -13,7 +16,10 @@ function App() {
           <Routes>
             <Route path='/' element={<HomeLoginPage />} />
             <Route path='/candidateInfo' element={<CandidateInfo />} />
+            <Route path='/MI' element={<MIView1 />} />
+            <Route path='/MI2' element={<MIView2 />} />
             <Route path='/vape' element={<VapeView />} />
+            <Route path='/drug' element={<DrugsView />} />
           </Routes>
         </header>
       </BrowserRouter>
